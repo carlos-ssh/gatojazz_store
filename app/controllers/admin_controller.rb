e4d1c375c99e1a912d84416081bc82380632bce2
Carlos-SSH <carlos.anezeh@me.com>
2024-03-06 19:10:44 -0600

    Create routes, views and a controller to handle admind

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,22 @@
+class AdminController < ApplicationController
+  before_action :authenticate_admin!
+  def index
+    # Your code here
+  end
+
+  def show
+    # Your code here
+  end
+
+  def create
+    # Your code here
+  end
+
+  def update
+    # Your code here
+  end
+
+  def destroy
+    # Your code here
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
   root to: "home#index"
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "admin" => "admin#index"
 end
